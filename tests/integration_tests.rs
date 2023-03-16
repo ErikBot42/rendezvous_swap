0c026870ecf367c3bec10cdb855907dc807bac16
Erik Magnusson <erik.arn.magnusson@gmail.com>
2023-03-16 17:27:44 +0100

    add data repeated test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -36,3 +36,27 @@ fn test_rendezvous_data() {
 
     handle.join().unwrap();
 }
+
+// repeatedly swap and modify data to see if something breaks
+#[test]
+fn test_rendezvous_data_repeat() {
+    const ITERATIONS: usize = 1000;
+    use rendezvous_swap::RendezvousData;
+    use std::thread;
+
+    let (mut my_rendezvous, mut their_rendezvous) = RendezvousData::new(0, 0);
+    let handle = thread::spawn(move || {
+        for _ in 0..ITERATIONS {
+            *their_rendezvous.swap() += 1;
+        }
+        their_rendezvous.swap();
+        their_rendezvous.swap();
+    });
+    for _ in 0..ITERATIONS {
+        *my_rendezvous.swap() += 1;
+    }
+    assert_eq!(*my_rendezvous.swap(), ITERATIONS);
+    assert_eq!(*my_rendezvous.swap(), ITERATIONS);
+
+    handle.join().unwrap();
+}
